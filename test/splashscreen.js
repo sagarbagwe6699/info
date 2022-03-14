5dfd86554d122cf4a1cc1ab3c7d706309308f0dd
Sagar Mahesh Bagwe <sbagwe@usc.edu>
2022-03-14 12:06:45 -0700

    test

---
+++ test/splashscreen.js
@@ -0,0 +1,9 @@
+function welcome_screen(){
+    // window.scrollTo(0, 0)
+    document.getElementById('w-s').style.opacity = '0'
+    document.getElementById('w-s').style.animation = 'ss 1s 1'
+    scrollTrigger('.my-card')
+    scrollTrigger('.card')
+    scrollTrigger('.skillbar')
+    scrollTrigger('.my-card-blue')
+}
